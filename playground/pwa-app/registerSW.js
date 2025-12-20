c038fc3b79cc2f2c182088602281ada539441762
stofu1234 <stofu1234@users.noreply.github.com>
2025-12-20 22:20:33 +0000

    deploy: 63d4404f3a27b57fe48dd620b6e81262f9c88d67

---
+++ playground/pwa-app/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/opal-vite/playground/pwa-app/sw.js', { scope: '/opal-vite/playground/pwa-app/' })})}
